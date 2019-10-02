3c2ef363834c92029b66ebfa878e8c1f28e1cb9f
FranKKastor <FranKKastor@users.noreply.github.com>
2019-10-02 19:56:35 -0300

    Archivo de parejas

--- TrabajoPractico.py
+++ TrabajoPractico.py
@@ -106,50 +106,50 @@ def Descartados (Diccionario,Condicion):
                 ListaDeTipo += [nombres]
         return ListaDeTipo
 
-def MatchearHeterosexuales (lista1,lista2,localidad):
-    ParejasFile = open("SalidaParejas.txt","w")
+def MatchearHeterosexuales (Archivo,lista1,lista2,localidad):
     while lista1!=[] and lista2!=[]:
-        EscribirParejas(ParejasFile,lista1[0],lista2[0],localidad)
+        EscribirParejas(Archivo,lista1[0],lista2[0],localidad)
         lista1.remove(lista1[0])
         lista2.remove(lista2[0])
-    ParejasFile.close()
 
-def MatchearHomosexuales (lista, localidad):
-    ParejasFile = open("SalidaParejas.txt","w")
+def MatchearHomosexuales (Archivo,lista, localidad):
+    
     while lista != [] and len(lista)!= 1:
-        EscribirParejas (ParejasFile,lista[0],lista[1],localidad)
+        EscribirParejas (Archivo,lista[0],lista[1],localidad)
         lista.remove(lista[0])
         lista.remove(lista[0])
-    ParejasFile.close()
 
-def Matchearbisexuales (lista, lista2, localidad):
+
+def Matchearbisexuales (Archivo,lista, lista2, localidad):
     while lista !=[] and lista2 !=[]:
-        EscribirParejas (ParejasFile, lista[0],lista2[0], localidad)
+        EscribirParejas (Archivo, lista[0],lista2[0], localidad)
         lista.remove(lista[0])
         lista2.remove(lista2[0])
-    ParejasFile.close()
+
 
 def Matching(Diccionario):
     PersonasUnicas = []
+    ParejasFile = open("SalidaParejas.txt","w")
     for Localidad in Diccionario.keys():
         if len(Diccionario[Localidad]) == 1:
             Persona = Diccionario[Localidad][0]
-            PersonasUnicas += [(Persona[0],Persona[1],Persona[2],Persona[3],Localidad)]
+            PersonasUnicas += [Persona[0]]
         else:
             ListaPorEdades = SepararPor(Diccionario[Localidad],"Edad")
             ListaPorEdades_Y_Sexo = []
             for Edad in ListaPorEdades:
                 ListaPorEdades_Y_Sexo += [SepararPor(Edad,"Genero")]
             for listaEdad in ListaPorEdades_Y_Sexo:
-                MatchearHeterosexuales(listaEdad[0],listaEdad[1],Localidad)
-                MatchearHomosexuales(listaEdad[2],Localidad)
-                MatchearHomosexuales(listaEdad[3],Localidad)
-                Matchearbisexuales(listaEdad[0],listaEdad[5],Localidad)
-                Matchearbisexuales(listaEdad[1],listaEdad[4],Localidad)
-                Matchearbisexuales(listaEdad[2],listaEdad[4],Localidad)
-                Matchearbisexuales(listaEdad[3],listaEdad[5],Localidad)
-                Matchearbisexuales(listaEdad[4],listaEdad[5],Localidad)
-
+                MatchearHeterosexuales(ParejasFile,listaEdad[0],listaEdad[1],Localidad)
+                MatchearHomosexuales(ParejasFile,listaEdad[2],Localidad)
+                MatchearHomosexuales(ParejasFile,listaEdad[3],Localidad)
+                Matchearbisexuales(ParejasFile,listaEdad[0],listaEdad[5],Localidad)
+                Matchearbisexuales(ParejasFile,listaEdad[1],listaEdad[4],Localidad)
+                Matchearbisexuales(ParejasFile,listaEdad[2],listaEdad[4],Localidad)
+                Matchearbisexuales(ParejasFile,listaEdad[3],listaEdad[5],Localidad)
+                Matchearbisexuales(ParejasFile,listaEdad[4],listaEdad[5],Localidad)
+    ParejasFile.close()
+    return PersonasUnicas
                 
 #FUNCION PRINCIPAL
 def Match ():
@@ -162,9 +162,10 @@ def Match ():
     NoParejasFile = open("SalidaNoParejas.txt","w")
     EscribirNoPareja(NoParejasFile,Diccionario_de_Personas,Menores_de_Edad,"Menores")
     EscribirNoPareja(NoParejasFile,Diccionario_de_Personas,Asexuales,"Asexuales")
-    NoParejasFile.close()
     Diccionario_de_Personas = EliminarDelDiccionario(Diccionario_de_Personas,Menores_de_Edad+Asexuales)
     DiccionarioPorLocalidades = Crear_Diccionario_de_Localidades(Diccionario_de_Personas)
-    Matching(DiccionarioPorLocalidades)
+    Unicos = Matching(DiccionarioPorLocalidades)
+    EscribirNoPareja(NoParejasFile,Diccionario_de_Personas,Unicos,"Unicos")
+    NoParejasFile.close()
 
 Match()
