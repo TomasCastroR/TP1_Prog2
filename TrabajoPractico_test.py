9b0e4dbc4255a02e85b94ca3f34ad6ba2017c6b2
FranKKastor <FranKKastor@users.noreply.github.com>
2019-10-08 20:26:12 -0300

    Terminado el comentado de los pytest

--- TrabajoPractico_test.py
+++ TrabajoPractico_test.py
@@ -1,14 +1,16 @@
 from TrabajoPractico import *
 
-""" El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de una lista de listas, que cada elemento sea un string
-    El archivo de resultados tiene un resultado por linea, y tiene que estar expresado en forma de una lista de quintuplas de strings """
+""" El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de una lista de listas de 6 strings con los datos de una persona
+    de la forma [Nombre, Apellido, Localidad, Edad, Genero, Interes] y pueden tener espacios al principio o final de cada string
+    El archivo de resultados tiene un resultado por linea, y tiene que estar expresado en forma de una lista de personas en quintuplas de strings
+    (NombreyApellido, Localidad, Edad, Genero, Interes) """
 def test_Pasar_a_Tupla ():
-    EjemplosFile = open("Pasar_a_TuplasEjemplo.txt","r")
-    ResultadosFile = open("Pasar_a_TuplasResultado.txt","r")
+    EjemplosFile = open("Pasar_a_TuplasEjemplos.txt","r")
+    ResultadosFile = open("Pasar_a_TuplasResultados.txt","r")
     LineasEjemplo = EjemplosFile.readlines()
     LineasResultados = ResultadosFile.readlines()
     EjemplosFile.close()
-    Resultados.close()
+    ResultadosFile.close()
     listaejemplo1 = eval(LineasEjemplo[0])
     listaejemplo2 = eval(LineasEjemplo[1])
     listaejemplo3 = eval(LineasEjemplo[2])
@@ -18,24 +20,26 @@ def test_Pasar_a_Tupla ():
     assert Pasar_a_Tupla(listaejemplo1) == listaresultado1
     assert Pasar_a_Tupla(listaejemplo2) == listaresultado2
     assert Pasar_a_Tupla(listaejemplo3) == listaresultado3
-"""El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de una lista de quintuplas de strings
+"""El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de una lista de personas en quintuplas con nombre y apellido, localidad, edad, genero e interes.
    El archivo de resultados tiene un resultado por linea, y tiene que estar expresado en forma de diccionario donde cada key es una localidad y su valor
-   asociado es una lista de cuatruplas"""
+   asociado es una lista de personas en cuatruplas con nombre y apellido, edad, genero e interes."""
 def test_Crear_Diccionario_de_Localidades ():
     EjemplosFile = open("Crear_Diccionario_de_LocalidadesEjemplos.txt","r")
     ResultadosFile = open("Crear_Diccionario_de_LocalidadesResultados.txt","r")
     LineasEjemplo = EjemplosFile.readlines()
     LineasResultados = ResultadosFile.readlines()
     EjemplosFile.close()
-    Resultados.close()
+    ResultadosFile.close()
     ListaTuplaEjemplo1 = eval(LineasEjemplo[0])
     ListaTuplaEjemplo2 = eval(LineasEjemplo[1])
     DiccionarioResultado1 = eval(LineasResultados[0])
     DiccionarioResultado2 = eval(LineasResultados[1])
     assert Crear_Diccionario_de_Localidades(ListaTuplaEjemplo1) == DiccionarioResultado1
     assert Crear_Diccionario_de_Localidades(ListaTuplaEjemplo2) == DiccionarioResultado2
-"""El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de lista de cuatruplas
-   El archivo de resultados tiene un resultado por linea, dependiendo del segundo argumento que puede ser "Edad" o "Genero" el resultado debe estar
+"""El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de lista de personas en cuatruplas con nombre y apellido, edad, genero e interes.
+   El archivo de resultados tiene un resultado por linea, dependiendo del segundo argumento que puede ser "Edad" o "Genero" el resultado debe estar:
+   En el primer caso, una lista de 3 listas de personas en cuatruplas con nombre y apellido, edad, genero e interes.
+   En el segundo, una lista de 6 listas de personas en cuatruplas con nombre y apellido, edad, genero e interes.
    """
 def test_SepararPor ():
     EjemplosFile = open("SepararPorEjemplos.txt","r")
@@ -43,7 +47,7 @@ def test_SepararPor ():
     LineasEjemplo = EjemplosFile.readlines()
     LineasResultados = ResultadosFile.readlines()
     EjemplosFile.close()
-    Resultados.close()
+    ResultadosFile.close()
     ListaEdadesEjemplo1 = eval(LineasEjemplo[0])
     ListaEdadesEjemplo2 = eval(LineasEjemplo[1])
     ListaGeneroInteresEjemplo1 = eval(LineasEjemplo[2])
@@ -56,14 +60,16 @@ def test_SepararPor ():
     assert SepararPor(ListaEdadesEjemplo2,"Edad") == ListaDeEdadesEsperada2
     assert SepararPor(ListaGeneroInteresEjemplo1,"Genero") == ListaGeneroInteresEsperada1
     assert SepararPor(ListaGeneroInteresEjemplo2,"Genero") == ListaGeneroInteresEsperada2
-
+"""El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de lista de personas en quintuplas de strings con nombre y apellido, localidad, edad, genero e interes.
+   El archivo de resultados tiene un resultado por linea, y tiene que estar expresado en forma de una lista de personas en quintuplas de strings
+   (NombreyApellido, Localidad, Edad, Genero, Interes)"""
 def test_Descartados ():
-    EjemplosFile = open("Pasar_a_TuplasEjemplo.txt","r")
-    ResultadosFile = open("Pasar_a_TuplasResultado.txt","r")
+    EjemplosFile = open("DescartadosEjemplos.txt","r")
+    ResultadosFile = open("DescartadosResultados.txt","r")
     LineasEjemplo = EjemplosFile.readlines()
     LineasResultados = ResultadosFile.readlines()
     EjemplosFile.close()
-    Resultados.close()
+    ResultadosFile.close()
     ListaPersonasEjemplo1 = eval(LineasEjemplo[0])
     ListaPersonasEjemplo2 = eval(LineasEjemplo[1])
     ListaDescartadosEsperada1 = eval(LineasResultados[0])
@@ -72,18 +78,21 @@ def test_Descartados ():
     assert Descartados(ListaPersonasEjemplo2,"Menores") == []
     assert Descartados(ListaPersonasEjemplo1,"Asexuales") == ListaDescartadosEsperada2
     assert Descartados(ListaPersonasEjemplo2,"Asexuales") == []
-
+"""El archivo de ejemplos tiene un ejemplo por linea, y tiene que estar expresado en forma de un diccionario donde las keys son localidades y sus valores asociados
+   lista de personas en cuatruplas de strings con nombre y apellido, edad, genero e interes.
+   El archivo de resultados tiene un resultado por linea, y tiene que estar expresado en forma de una lista de personas en quintuplas de strings
+   (NombreyApellido, Localidad, Edad, Genero, Interes)"""
 def test_Matching ():
     EjemplosFile = open("MatchingEjemplos.txt","r")
-    ResultadosFile = open("MatchingResultados.txt","r")
+    ResultadosFile = open("MatchingResultadados.txt","r")
     LineasEjemplo = EjemplosFile.readlines()
     LineasResultados = ResultadosFile.readlines()
     EjemplosFile.close()
-    Resultados.close()
+    ResultadosFile.close()
     DiccionarioEjemplo1 = eval(LineasEjemplo[0])
     DiccionarioEjemplo2 = eval(LineasEjemplo[1])
-    ListaResultado1 = eval(LineasResultado[0])
-    ListaResultado2 = eval(LineasResultado[1])
+    ListaResultado1 = eval(LineasResultados[0])
+    ListaResultado2 = eval(LineasResultados[1])
 
     assert Matching(DiccionarioEjemplo1) == ListaResultado1
     assert Matching(DiccionarioEjemplo2) == ListaResultado2
--- ejemplo.py
+++
@@ -1,4 +0,0 @@
-EjemploFile = open("Pasar_a_TuplasEjemplos.txt","r")
-Blas = eval(EjemploFile.read())
-EjemploFile.close()
-print(Blas)
